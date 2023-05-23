b3a4613ecd9c881478e4e85260b722af488d650e
Grammeri <89168093+Grammeri@users.noreply.github.com>
2023-05-23 16:07:30 -0500

    draft chatSlice

--- src/features/chat/chatSlice.js
+++ src/features/chat/chatSlice.js
@@ -1,1 +1,33 @@
+// src/features/chat/chatSlice.js
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+    chats: [],
+    currentChat: null,
+};
+
+const chatSlice = createSlice({
+    name: 'chat',
+    initialState,
+    reducers: {
+        setChats: (state, action) => {
+            state.chats = action.payload;
+        },
+        setCurrentChat: (state, action) => {
+            state.currentChat = action.payload;
+        },
+        sendMessage: (state, action) => {
+            const { chatId, message } = action.payload;
+            const chat = state.chats.find((chat) => chat.id === chatId);
+            if (chat) {
+                chat.messages.push(message);
+            }
+        },
+        // Add more actions as needed...
+    },
+});
+
+export const { setChats, setCurrentChat, sendMessage } = chatSlice.actions;
+
+export default chatSlice.reducer;
 
