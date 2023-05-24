6c186cb5beb44054489f4f0e92fdc184c808297a
Grammeri <89168093+Grammeri@users.noreply.github.com>
2023-05-23 21:16:41 -0500

    draft logout

---
+++ src/features/auth/components/logout/Logout.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Logout = () => {
+    // Logic for handling logout
+
+    return (
+        <button onClick={handleLogout}>
+            Logout
+        </button>
+    );
+};
+
+export default Logout;
