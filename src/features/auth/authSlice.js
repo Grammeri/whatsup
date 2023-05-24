4f05fbf2251be1517c36402680e8f41d1603ee64
Grammeri <89168093+Grammeri@users.noreply.github.com>
2023-05-24 10:44:19 -0500

    mock login in authSlice

--- src/features/auth/authSlice.js
+++ src/features/auth/authSlice.js
@@ -1,79 +1,29 @@
-/*
 import { createSlice } from '@reduxjs/toolkit';
 
-
-
-export const login = createAsyncThunk(
-    'auth/login',
-    async (loginData, { rejectWithValue }) => {
-        try {
-            const response = await authAPI.login(loginData);
-            // Handle successful login
-            return response.data;
-        } catch (error) {
-            // Handle login error
-            return rejectWithValue(error.response.data);
-        }
-    }
-);
-
-export const logout = createAsyncThunk('auth/logout', async () => {
-    try {
-        const response = await authAPI.logout();
-        // Handle successful logout
-        return response.data;
-    } catch (error) {
-        // Handle logout error
-        return error.response.data;
-    }
-});
+const initialState = {
+    isLoggedIn: false,
+    loginError: null,
+};
 
 const authSlice = createSlice({
     name: 'auth',
-    initialState: {
-        isLoggedIn: false,
-        user: null,
-        error: null,
-        isLoading: false,
-    },
+    initialState,
     reducers: {
-        setIsLoggedIn: (state, action) => {
-            state.isLoggedIn = action.payload;
-        },
-    },
-    extraReducers: (builder) => {
-        builder
-            .addCase(login.pending, (state) => {
-                state.isLoading = true;
-                state.error = null;
-            })
-            .addCase(login.fulfilled, (state, action) => {
-                state.isLoading = false;
+        login: (state, action) => {
+            const { username, password } = action.payload;
+            if (username === "You are always welcome!" && password === "No passwords required!") {
                 state.isLoggedIn = true;
-                state.user = action.payload.user;
-            })
-            .addCase(login.rejected, (state, action) => {
-                state.isLoading = false;
-                state.error = action.payload.error;
-            })
-            .addCase(logout.pending, (state) => {
-                state.isLoading = true;
-                state.error = null;
-            })
-            .addCase(logout.fulfilled, (state) => {
-                state.isLoading = false;
-                state.isLoggedIn = false;
-                state.user = null;
-            })
-            .addCase(logout.rejected, (state, action) => {
-                state.isLoading = false;
-                state.error = action.payload.error;
-            });
-    },
+                state.loginError = null;
+            } else {
+                state.loginError = "Oops! Please enter the correct greeting.";
+            }
+        },
+        logout: (state) => {
+            state.isLoggedIn = false;
+        }
+    }
 });
 
-export const authReducer = slice.reducer
-export const { setIsLoggedIn } = authSlice.actions;
+export const { login, logout } = authSlice.actions;
+
 export default authSlice.reducer;
-*/
-export {}
---
+++ src/features/auth/components/authApi.js
@@ -0,0 +1,14 @@
+export const authApi = {
+    login: (idInstance, apiTokenInstance) => {
+        // replace these with real validations if needed
+        if (idInstance && apiTokenInstance) {
+            return Promise.resolve({ isLoggedIn: true });
+        } else {
+            return Promise.reject({ message: 'Login failed.' });
+        }
+    },
+    logout: () => {
+        // replace this with real logout logic if needed
+        return Promise.resolve({ isLoggedIn: false });
+    },
+};
